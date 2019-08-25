030ff2f8854b3401014eb46e0f5faf51e87c3e4e
dudy0127 <dudy0127@gmail.com>
2019-08-25 21:07:11 +0800

    spring cloud stream

--- stream-sink/src/main/java/io/github/com/dudy/springcloud/streamsink/stream/GreetingsStreams.java
+++ stream-sink/src/main/java/io/github/com/dudy/springcloud/streamsink/stream/GreetingsStreams.java
@@ -6,11 +6,11 @@ import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.SubscribableChannel;
 
 public interface GreetingsStreams {
-    String INPUT = "greetings-in";
+    //String INPUT = "greetings-in";
     String OUTPUT = "greetings-out";
 
-    @Input(INPUT)
-    SubscribableChannel inboundGreetings();
+//    @Input(INPUT)
+//    SubscribableChannel inboundGreetings();
 
     @Output(OUTPUT)
     MessageChannel outboundGreetings();
